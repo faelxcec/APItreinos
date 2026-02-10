1089dd6214ed56e3e317dbff5891a9015a9dc528
Rafael <rafaben2013@gmail.com>
2026-02-10 01:51:33 -0300

    0.0.0

---
+++ src/test/java/com/APItreino/Treinos/TreinosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.APItreino.Treinos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreinosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
